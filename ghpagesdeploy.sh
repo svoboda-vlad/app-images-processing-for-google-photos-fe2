5041728d8f9fd8ef80d323fea13220ac8ff890c9
Vladimír Svoboda <svoboda-vlad@seznam.cz>
2021-10-22 06:16:57 +0000

    ghpagesdeploy updated

--- ghpagesdeploy.sh
+++ ghpagesdeploy.sh
@@ -2,7 +2,7 @@ echo "pulling master branch from origin" &&
 git checkout master &&
 git pull origin master &&
 echo "project buidling started" &&
-ng build --output-path docs --base-href "https://svoboda-vlad.github.io/app-images-processing-for-google-photos-fe/" &&
+ng build --prod --output-path docs --base-href "https://svoboda-vlad.github.io/app-images-processing-for-google-photos-fe/" &&
 cp docs/index.html docs/404.html &&
 echo "project building finished" &&
 echo "git commiting started" &&

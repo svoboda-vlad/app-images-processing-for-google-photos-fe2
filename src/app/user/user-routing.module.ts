53861e4b21c05db24132d39a6af51b6ae4eac4cf
Vladimír Svoboda <svoboda-vlad@seznam.cz>
2021-12-25 23:30:10 +0100

    user edit removed

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserDetailComponent } from '../user-detail/user-detail.component';
-import { UserEditComponent } from '../user-edit/user-edit.component';
 
 const routes: Routes = [{
   path: '',
   component: UserDetailComponent
-},
-{
-  path: 'edit',
-  component: UserEditComponent
 }];
 
 @NgModule({

5a5e3b4a8803aea6d7386435642615b92fb39b8b
Vladimír Svoboda <svobo@SVOBODAITNB.koposko.local>
2022-06-19 11:58:33 +0200

    back-end URLs changed, staging config added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  SERVER_URL: 'https://images-proc-for-google-photos.herokuapp.com/',
+  SERVER_URL: 'https://processing-gphotos.herokuapp.com/',
   GOOGLE_CLIENT_ID: '733460469950-rnm4b6pek82bfrnd8f5hf5esa5an0ikk.apps.googleusercontent.com',
   FRONT_END_URL: 'https://svoboda-vlad.github.io/app-images-processing-for-google-photos-fe/'
 };

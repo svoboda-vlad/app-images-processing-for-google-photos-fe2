9f3db3ff830bc10e617986acc6039cfaee6b93cc
Vladimír Svoboda <svobo@SVOBODAITNB.koposko.local>
2022-06-19 12:00:05 +0200

    GitHub pages deployment

--- docs/main.37d20672a11ba946.js
+++ docs/main.37d20672a11ba946.js
@@ -1276,7 +1276,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 const environment = {
   production: true,
-  SERVER_URL: 'https://images-proc-for-google-photos.herokuapp.com/',
+  SERVER_URL: 'https://processing-gphotos.herokuapp.com/',
   GOOGLE_CLIENT_ID: '733460469950-rnm4b6pek82bfrnd8f5hf5esa5an0ikk.apps.googleusercontent.com',
   FRONT_END_URL: 'https://svoboda-vlad.github.io/app-images-processing-for-google-photos-fe/'
 };
